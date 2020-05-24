3d5df25291895d85cfe902a5c8a2d840ec87332b
przemas1111 <przemyslaw.malinski2@wp.pl>
2020-05-24 16:56:45 +0200

    ProductForm separated from AdminPanel

---
+++ client/src/components/ProductForm.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="product-form">
+    <div class="form-group">
+      <div class="row justify-content-md-center">
+        <div class="col-4">
+          <label>Nazwa</label>
+          <input type="text" class="form-control" placeholder="Nazwa" v-model="productData.name" @keyup.enter="confirm">
+        </div>
+
+        <div class="col-2">
+          <label>Cena</label>
+          <input type="number" class="form-control" placeholder="Cena" v-model="productData.price" @keyup.enter="confirm">
+        </div>
+      </div>
+      <div class="row justify-content-md-center">
+        <div class="col-6">
+          <label>Opis</label>
+          <input type="text" class="form-control" placeholder="Opis" v-model="productData.description" @keyup.enter="confirm">
+        </div>
+      </div>
+      <div class="row justify-content-md-center">
+        <div class="col-6">
+          <label>Dostawa</label>
+          <input type="text" class="form-control" placeholder="Dostawa" v-model="productData.shipping" @keyup.enter="confirm">
+        </div>
+      </div>
+      <div class="row justify-content-md-center">
+        <div class="col-2">
+          <label>Gwarancja (miesiące)</label>
+          <input type="number" class="form-control" placeholder="Miesiące" v-model="productData.warranty" @keyup.enter="confirm">
+        </div>
+        <div class="col-2">
+          <label>Ilość</label>
+          <input type="number" class="form-control" placeholder="Ilość" v-model="productData.quantity" @keyup.enter="confirm">
+        </div>
+      </div>
+
+      <div class="buttons">
+        <button type="button" class="btn btn-primary" @click="confirm">Potwierdź</button>
+        <button type="button" class="btn btn-primary" @click="cancel">Anuluj</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default {
+  name: 'ProductForm',
+  props: {
+    product: {
+      name: '',
+      price: null,
+      description: '',
+      shipping: '',
+      warranty: null,
+      quantity: null
+    }
+  },
+  data: function () {
+    return {
+      productData: Vue.util.extend({}, this.product)
+    }
+  },
+  methods: {
+    confirm: function () {
+      this.$emit('confirm', this.productData)
+      this.$emit('cancel')
+    },
+    cancel: function () {
+      this.$emit('cancel')
+      this.productData.name = ''
+      this.productData.price = null
+      this.productData.description = ''
+      this.productData.shipping = ''
+      this.productData.warranty = null
+      this.productData.quantity = null
+    }
+  }
+}
+</script>
+
+<style scoped>
+.form-control{ margin-bottom: 10px; text-align: center; }
+
+.btn{ margin-bottom: 10px; }
+
+.buttons{ margin-top: 30px; }
+</style>
