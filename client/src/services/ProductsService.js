5900483e3e1753acf9797e8a5f6b27d2dba2aaa1
przemas1111 <przemyslaw.malinski2@wp.pl>
2020-05-24 16:53:23 +0200

    ProductService REST API finished

--- client/src/services/ProductsService.js
+++ client/src/services/ProductsService.js
@@ -2,12 +2,25 @@ import Api from '@/services/Api'
 
 export default {
   fetchProducts: function () {
-    return Api().get('products').then(response => { return response.data })
+    return Api().get('api/products')
+      .then(response => { return response.data })
       .catch(error => console.log(error))
   },
 
   addProduct (params) {
-    return Api().post('products', params)
+    return Api().post('api/products', params)
+      .catch(error => console.log(error))
+  },
+
+  editProduct (params) {
+    return Api().put('api/products/' + params._id, params)
+      .then(response => { return response.data })
+      .catch(error => console.log(error))
+  },
+
+  deleteProduct (params) {
+    return Api().delete('api/products/' + params._id)
+      .then(response => { return response.data })
       .catch(error => console.log(error))
   }
 }
